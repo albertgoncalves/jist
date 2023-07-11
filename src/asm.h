398e66225cf0d9ba367e63a828234d695c206141
albertgoncalves <goncalves.albert@gmail.com>
2023-07-10 21:45:23 -0400

    remove dupe `extern`s

--- src/asm.h
+++ src/asm.h
@@ -7,10 +7,4 @@ void  asm_emit(void);
 void* asm_jit(void);
 void  asm_show(void);
 
-extern const char* ESCAPES[CAP_ESCAPES];
-extern u32         LEN_ESCAPES;
-
-extern const Expr* LIST[CAP_LIST];
-extern u32         LEN_LIST;
-
 #endif
