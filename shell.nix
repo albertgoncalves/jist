3452f239054934796d5da8ee82813b09da69d923
albertgoncalves <goncalves.albert@gmail.com>
2024-10-14 15:09:53 -0400

    update nix dev env

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
-mkShell.override { stdenv = llvmPackages_16.stdenv; } {
+mkShell.override { stdenv = llvmPackages_18.stdenv; } {
     buildInputs = [
         mold
     ];
